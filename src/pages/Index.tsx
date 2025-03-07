a5baf388a72704b81f012951dceb6e2789392095
AndileTL <amoyo700@gmail.com>
2025-03-07 08:58:32 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -55,21 +55,30 @@ const Home = () => {
 
   const teamPerformance = [
     {
-      name: "Team Alpha",
+      name: "Team Andile",
       aht: "275s",
       csat: "94%",
       fcr: "87%",
       sla: "82%"
     },
     {
-      name: "Team Beta",
+      name: "Team Blessing",
       aht: "290s",
       csat: "91%",
       fcr: "82%",
       sla: "78%"
     },
     {
-      name: "Team Gamma",
+      name: "Donald",
+      aht: "285s",
+      csat: "89%",
+      fcr: "84%",
+      sla: "81%"
+    },
+  ];
+
+  {
+      name: "Mildred",
       aht: "285s",
       csat: "89%",
       fcr: "84%",
